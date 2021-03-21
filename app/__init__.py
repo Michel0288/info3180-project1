5fb5394761c81f8cc3f692d25457b478a71bf70d
Michel Davies <micheldavies0288@gmail.com>
2021-03-20 22:23:42 -0500

    heroku fix attempt

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from .config import Config
 app = Flask(__name__)
-
 db = SQLAlchemy(app)
 
 app.config.from_object(Config)
-from app import views,models
+from app import views
